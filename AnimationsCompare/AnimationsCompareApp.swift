59d62b3286d2e97c81b6190aa46e61fa7a09b0f4
Carlos Kimura <carloskimura@Carloss-MacBook-Pro.local>
2022-12-14 20:43:41 -0300

    initial project files

---
+++ AnimationsCompare/AnimationsCompareApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsCompareApp.swift
+//  AnimationsCompare
+//
+//  Created by Carlos Kimura on 14/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsCompareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationsCompare/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AnimationsCompare
+//
+//  Created by Carlos Kimura on 14/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
